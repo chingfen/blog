ee674fc3cfe3903b91ca768fb69ee7817734fa69
wcf12345 <wl1i56665545@gmail.com>
2017-12-13 14:55:06 +0800

    end of ch7

---
+++ blog/article/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+from article.models import Article, Comment
+
+
+def article(request):
+    '''
+    Render the article page
+    '''
+    articles = Article.objects.all()
+    context = {'articles':articles}
+    return render(request, 'article/article.html',context)
---
+++ blog/main/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+import datetime
+def main(request):
+    '''
+    Render the main page
+    '''
+    context = {'like':'Django 很棒'}
+    context.update({ 'now' : datetime.datetime.now()})
+    return render(request, 'main/main.html', context)
+def about(request):
+    '''
+    Render the about page
+    '''
+    return render(request, 'main/about.html')
